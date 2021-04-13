e48152cca68b229bd968f7ce33e2d5df5a0a655f
DavidRMorphew <morphewd@umich.edu>
2021-04-12 21:35:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return (string[0].toUpperCase() + string.slice(1))
+  };
+  static sanitize(){};
+  static titleize(sentence){
+    const split = sentence.split(" ")
+    // return split.forEach(function(el){this.capitalize(el)}).join(" ")
+  };
 }
