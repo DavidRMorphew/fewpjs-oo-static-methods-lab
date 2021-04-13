b188009b59a1cb5afbf8925a19ac0a5466f770af
DavidRMorphew <morphewd@umich.edu>
2021-04-13 08:20:35 -0400

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ class Formatter {
   static capitalize(string){
     return (string[0].toUpperCase() + string.slice(1))
   };
-  static sanitize(){};
+  static sanitize(string){
+    const split = string.split("").filter(el => el.match(/[-A-Za-z ']/g));
+    return split.join("")
+  };
+  
   static titleize(sentence){
     const split = sentence.split(" ")
-    // return split.forEach(function(el){this.capitalize(el)}).join(" ")
+    const articles = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const capitalizeElement = this.capitalize;
+    return split.map((el) => {
+        if (split[0] === el){
+          return capitalizeElement(el);
+        } else if (articles.some(article => article === el)){
+          return el;
+        } else { 
+          return capitalizeElement(el);
+        }
+    }).join(" ");
   };
 }
